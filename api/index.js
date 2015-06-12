1c7e17f0cb49e61642be05a72f4209ea04a6177a
Antoine <NattackDev@users.noreply.github.com>
2015-06-12 22:22:21 +0200

    Create index.js

---
+++ api/index.js
@@ -0,0 +1,5 @@
+var fs = require("fs");
+SupAPI.registerPlugin("typescript", "Sup.MathPlus", {
+    defs: fs.readFileSync(__dirname + "/Sup.MathPlus.d.ts.txt", { encoding: "utf8" }),
+    code: fs.readFileSync(__dirname + "/Sup.MathPlus.ts.txt", { encoding: "utf8" }),
+});
